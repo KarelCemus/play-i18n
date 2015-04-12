e11e337f3907c22d312e2704af1ebf9b0b417bc4
Karel Cemus <karel.cemus@gmail.com>
2015-04-12 18:28:43 +0200

    #1 YAML test cases

--- src/test/scala/play/api/i18n/MultiFormatMessagingPluginSpec.scala
+++ src/test/scala/play/api/i18n/MultiFormatMessagingPluginSpec.scala
@@ -1,21 +1,47 @@
 package play.api.i18n
 
 import org.specs2.mutable.Specification
-import play.api.test.WithApplication
+import org.specs2.specification._
+import play.api.Play
+import play.api.test._
 
-class MultiFormatMessagingPluginSpec extends Specification {
+class MultiFormatMessagingPluginSpec extends Specification with BeforeAfterAll with Scope {
 
-  val default = play.api.i18n.Lang( "fr" )
+  override def beforeAll( ): Unit = Play.start( FakeApplication( ) )
+
+  override def afterAll( ): Unit = Play.stop( )
+
+  implicit val default = play.api.i18n.Lang( "fr" )
+
   val cs = play.api.i18n.Lang( "cs" )
+
   val en = play.api.i18n.Lang( "en" )
 
   "Plugin" should {
 
-    "read property files" in new WithApplication( ) {
+    "read property files" in {
+      Messages( "a.b.c", 1, 2, 3 )( default ) mustEqual "a=1 b=2 c=3"
+      Messages( "a.b.c", 1, 2, 3 )( cs ) mustEqual "a=1 b=2 c=3 in cs"
+      Messages( "a.b.c", 1, 2, 3 )( en ) mustEqual "a=1 b=2 c=3 in en"
+    }
 
-      Messages( "a.b.c", 1, 2, 3 )(default) mustEqual "a=1 b=2 c=3"
-      Messages( "a.b.c", 1, 2, 3 )(cs) mustEqual "a=1 b=2 c=3 in cs"
-      Messages( "a.b.c", 1, 2, 3 )(en) mustEqual "a=1 b=2 c=3 in en"
+    "read YAML files" in {
+      Messages( "a.b.d", 1, 2, 3 )( default ) mustEqual "a=1 b=2 c=3"
+      Messages( "a.b.d", 1, 2, 3 )( cs ) mustEqual "a=1 b=2 c=3 in cs"
+      Messages( "a.b.d", 1, 2, 3 )( en ) mustEqual "a=1 b=2 c=3 in en"
+    }
+
+    "implement advanced YAML features: hierarchy" in {
+      Messages( "a.x" ) mustEqual "This is an example"
+    }
+
+    "implement advanced YAML features: multi-line strings" in {
+      Messages( "a.f" ) mustEqual "This\nis\nan\nexample"
+    }
+
+    "implement advanced YAML features: references" in {
+      Messages( "a.e.a" ) mustEqual "An example"
+      Messages( "a.g.a" ) mustEqual "An example"
     }
   }
 }
