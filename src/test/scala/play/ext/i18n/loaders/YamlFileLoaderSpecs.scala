da03fb04443be18e5b6826143e2cd9c104beff84
Karel Cemus <karel.cemus@gmail.com>
2015-04-15 09:11:57 +0200

    #4 Support of empty yaml files - included test case reproducing the bug

---
+++ src/test/scala/play/ext/i18n/loaders/YamlFileLoaderSpecs.scala
@@ -0,0 +1,26 @@
+package play.ext.i18n.loaders
+
+import scala.collection.JavaConverters._
+
+import play.api.i18n.Messages.{MessageSource, UrlMessageSource}
+
+import org.specs2.mutable.Specification
+
+class YamlFileLoaderSpecs extends Specification {
+
+  "YAML parser" should {
+
+    val parser = new YamlFileLoader()
+
+    "handle empty message file" in {
+      parser(new MessageSource {
+        override def read: String = ""
+      }, "empty") mustEqual Right(Map.empty)
+    }
+
+    "handle non-empty message file" in {
+      val resource = new UrlMessageSource(this.getClass.getClassLoader.getResources("messages.yaml.cs").asScala.toList.head)
+      parser(resource, "messages.yaml") mustEqual Right(Map("a.b.d" -> "a={0} b={1} c={2} in cs"))
+    }
+  }
+}
